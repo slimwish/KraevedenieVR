6bcf83ac1acebd8b091c6f8cc076a705f31f9e45
slimwish <salyami811@gmail.com>
2025-04-05 10:11:55 +1000

    update: image

--- Assets/Resources/Scripts/PanoTourScripts/PointHandler.cs
+++ Assets/Resources/Scripts/PanoTourScripts/PointHandler.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointHandler : Interacting
 {
     [SerializeField] GameObject _sphere;
+    [SerializeField] Image _image;
 
     public override void Interact(GameObject getGameObject)
     {
@@ -13,12 +15,12 @@ public class PointHandler : Interacting
 
     public override void InteractRay(GameObject getGameObject)
     {
-
+        _image.color = new Color(255, 255, 255, 1);
     }
 
     public override void InteractStopRay(GameObject getGameObject)
     {
-
+        _image.color = new Color(150, 150, 150, 0.7f);
     }
 
     public override void InteractTriggerRay(GameObject getGameObject)
