29ac422b370d8c0da03961c3c2d2d31e3ffc5a88
slimwish <salyami811@gmail.com>
2025-05-19 18:30:55 +1000

    meow

--- Assets/Resources/Scripts/PanoTourScripts/PanoTourInterface.cs
+++ Assets/Resources/Scripts/PanoTourScripts/PanoTourInterface.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PanoTourInterface : MonoBehaviour
 {
-    [SerializeField] private Button _button;
+    [SerializeField] private InputActionProperty _menuButton;
     [SerializeField] private string _sceneName;
+    private bool _status = false;
 
+    private void Start()
+    {
+        _menuButton.action.started += ButtonWasPressed;
+    }
+    void ButtonWasPressed(InputAction.CallbackContext context)
+    {
+        gameObject.SetActive(_status);
+        _status = !_status;
+    }
     public void LoadScene()
     {
         StartCoroutine(LoadSceneTerminalAsync());
